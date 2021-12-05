b53a7712212fe991bd16fbbe9bfdffe77792224a
MikeEckels <32800922+MikeEckels@users.noreply.github.com>
2021-12-05 00:22:36 -0500

    Fixed bug with Xwindows backend

--- src/Interface.py
+++ src/Interface.py
@@ -1,4 +1,9 @@
 import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
+import matplotlib.pyplot as plt
 import sys
 import numpy
 import time
